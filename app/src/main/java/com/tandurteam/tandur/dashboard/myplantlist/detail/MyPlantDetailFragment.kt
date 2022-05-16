6ea468932177753c86e2a8563d3cdb5759295518
iqbalShafiq <iqbal.syafiq.rozaan@gmail.com>
2022-05-16 10:48:19 +0700

    Create planting nav graph

--- app/src/main/java/com/tandurteam/tandur/dashboard/myplantlist/detail/MyPlantDetailFragment.kt
+++ app/src/main/java/com/tandurteam/tandur/dashboard/myplantlist/detail/MyPlantDetailFragment.kt
@@ -1,10 +1,10 @@
-package com.tandurteam.tandur.plant.detail
+package com.tandurteam.tandur.dashboard.myplantlist.detail
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.tandurteam.tandur.R
 
 // TODO: Rename parameter arguments, choose names that match
