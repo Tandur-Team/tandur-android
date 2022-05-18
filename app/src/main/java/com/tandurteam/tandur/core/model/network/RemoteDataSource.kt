c4288c475c9e204c9df135983a976091673b4e59
iqbalShafiq <iqbal.syafiq.rozaan@gmail.com>
2022-05-18 11:48:58 +0700

    Create remote data source for login user

--- app/src/main/java/com/tandurteam/tandur/core/model/network/RemoteDataSource.kt
+++ app/src/main/java/com/tandurteam/tandur/core/model/network/RemoteDataSource.kt
@@ -1,7 +1,9 @@
 package com.tandurteam.tandur.core.model.network
 
 import android.util.Log
+import com.tandurteam.tandur.core.model.network.authentication.request.LoginRequest
 import com.tandurteam.tandur.core.model.network.authentication.request.SignUpRequest
+import com.tandurteam.tandur.core.model.network.authentication.response.LoginResponse
 import com.tandurteam.tandur.core.model.network.authentication.response.SignUpResponse
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
@@ -30,8 +32,31 @@ class RemoteDataSource(private val apiService: ApiService) {
         }
     }
 
+    suspend fun loginUser(loginRequest: LoginRequest): ApiResponse<LoginResponse>? {
+        return try {
+            val response = withContext(Dispatchers.IO) {
+                apiService.loginUser(loginRequest)
+            }
+
+            when (response.message) {
+                AUTH_SUCCESS -> {
+                    ApiResponse.Success(response)
+                }
+                else -> {
+                    response.message?.let {
+                        ApiResponse.Error(it)
+                    }
+                }
+            }
+        } catch (e: Exception) {
+            Log.e(TAG, "signUpUser: $e")
+            ApiResponse.Error(e.toString())
+        }
+    }
+
     companion object {
         private val TAG = RemoteDataSource::class.java.simpleName
         private const val USER_CREATED = "User Created"
+        private const val AUTH_SUCCESS = "Auth Success"
     }
 }
