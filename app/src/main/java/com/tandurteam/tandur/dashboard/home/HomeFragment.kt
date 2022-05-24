efba899f088115c5d407f580652bf78575abb4c9
Bagas Wahyu Andrian <76098236+bagasandriann@users.noreply.github.com>
2022-05-24 18:19:21 +0700

    Adding adapter and API integration to HomeFragment.kt, but still an error

--- app/src/main/java/com/tandurteam/tandur/dashboard/home/HomeFragment.kt
+++ app/src/main/java/com/tandurteam/tandur/dashboard/home/HomeFragment.kt
@@ -5,14 +5,21 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.tandurteam.tandur.core.adapter.FixedPlantAdapter
+import com.tandurteam.tandur.core.model.network.ApiResponse
 import com.tandurteam.tandur.databinding.FragmentHomeBinding
 import com.tandurteam.tandur.maps.MapsActivity
 
+
 class HomeFragment : Fragment() {
 
+    private val viewModel: HomeViewModel by viewModels()
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
+    private lateinit var adapter: FixedPlantAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -29,6 +36,41 @@ class HomeFragment : Fragment() {
         binding.tvLocation.setOnClickListener {
             Intent(requireActivity(), MapsActivity::class.java).apply {
                 requireActivity().startActivity(this)
+            }
+        }
+
+        // init adapter
+        adapter = FixedPlantAdapter()
+        adapter.onItemClick = { fixedPlant ->
+            // TODO: On item clicked should be go to detail
+        }
+
+        // observe live data
+        observeLiveData()
+    }
+
+    private fun observeLiveData() {
+        viewModel.getAllFixedPlant().observe(viewLifecycleOwner) {
+            it?.let { fixedPlant ->
+                when (fixedPlant) {
+                    is ApiResponse.Success -> {
+                        // set adapter of rvApaYangMerekaTanam (Fixed Plant)
+                        adapter.setData(fixedPlant.data.data)
+                        binding.rvApaYangMerekaTanam.apply {
+                            setHasFixedSize(true)
+                            adapter = this@HomeFragment.adapter
+                        }
+
+                    }
+                    else -> {
+                        // Toast message error
+                        Toast.makeText(
+                            requireContext(),
+                            "Gagal Memuat Tanaman",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
             }
         }
     }
